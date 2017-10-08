48ff2a9c77ec05e2a771efb8999679698d2c6914
Rogers <r@allur.co>
2017-10-08 18:50:16 -0300

    Resume

--- resume/index.ios.js
+++ resume/index.ios.js
@@ -9,26 +9,86 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
+import { Accelerometer, Gyroscope } from 'react-native-sensors';
+
+const gyroscopeObservable = new Gyroscope({
+    updateInterval: 300, // defaults to 100ms
+});
 
 export default class resume extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            acceleration: {
+                x: 'unknown',
+                y: 'unknown',
+                z: 'unknown',
+            },
+            gyroscope: {
+                x: 'unknown',
+                y: 'unknown',
+                z: 'unknown',
+            }
+        };
+    }
+
+    componentWillMount() {
+
+        gyroscopeObservable
+            .subscribe(gyroscope => this.setState({
+                gyroscope,
+            }));
+    }
+
+    chooseImage(x, y) {
+
+        let images = [require('./images/forward.png'), require("./images/left.png"), require("./images/right.png"), require("./images/top.png"), require("./images/bottom.png")];
+
+        if (x < -0.04 && y < -0.05) {
+            return images[0];
+        }
+
+        if (x < -0.06) {
+            return images[1];
+        }
+
+        if (x > 0.06) {
+            return images[2];
+        }
+
+        if (y > 0) {
+            return images[3];
+        }
+
+        if (y < 0) {
+            return images[4];
+        }
+
+        return images[0];
+
+    }
+
   render() {
+      const {
+          gyroscope,
+      } = this.state;
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+
+            <Image style={styles.image} source={this.chooseImage(gyroscope.x, gyroscope.y)} resizeMode="contain" />
+
+        </View>
     );
   }
+
+    componentWillUnmount() {
+        gyroscopeObservable.stop();
+    }
 }
 
 const styles = StyleSheet.create({
@@ -36,7 +96,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000',
   },
   welcome: {
     fontSize: 20,
@@ -45,9 +105,12 @@ const styles = StyleSheet.create({
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#fff',
     marginBottom: 5,
   },
+    image: {flex: 1,
+        alignSelf: 'center'}
 });
+
 
 AppRegistry.registerComponent('resume', () => resume);
